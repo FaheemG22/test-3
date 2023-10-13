0f024aa339792041a86516ad31240c4459b1cf98
FaheemG22 <119604215+FaheemG22@users.noreply.github.com>
2023-10-13 22:34:40 +0100

    improvements to mobile responsiveness

--- header.php
+++ header.php
@@ -13,12 +13,12 @@ session_start();
 <link rel="stylesheet" href="assets/css/custom.css">
 
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="color:white;min-height:100px;height:20vw;max-height:150px">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="color:white;min-height:100px;height:20vw;max-height:150px;font-size:2vw;">
   <div class="container-fluid  bg-dark"> 
-    <a class="navbar-brand" href="index.php">Test-Signon - V0.4</a>
+    <a class="navbar-brand" style="font-size:2vw;" href="index.php">Test-Signon - V0.4</a>
 
 
-    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100%;">
+    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100%;font-size:1.5vw;">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="securePage.php">Secure</a>
         </li>
@@ -43,36 +43,33 @@ session_start();
               $_SESSION['register'] = 'n'; 
           } 
 
-          //Logged in
-          if (isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
+          if (isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {//Logged in
           echo 'Logged in as : ' . $_SESSION['name'] . ' ';
           echo '<input class="btn btn-outline-danger" type="submit" value="Logout" formaction="logout.php">';
           }
-          //Registry
-          elseif (isset($_SESSION['register']) && $_SESSION['register'] == 'y') {
+          elseif (isset($_SESSION['register']) && $_SESSION['register'] == 'y') {//Registry
           echo '
           <input class="input-header" type="text" name="userid" id="userid" placeholder="Username" required>
           <input class="input-header" type="text" name="email" id="email" placeholder="Email" required>
           <input class="input-header" type="password" name="password" id="password" placeholder="Password" required>
-          <div>
-          <input class="btn btn-success custom" type="submit" value="Register" formaction="submit.php">
+          <div class="flex-container">
+          <div><input class="btn btn-success custom" type="submit" value="Register" formaction="submit.php"></div>
           </form>
           <form method="post">
-          <button class="btn btn-outline-danger custom" type="submit" name="unreg"> Cancel </button>
+          <div><button class="btn btn-outline-danger custom" type="submit" name="unreg"> Cancel </button></div>
           </form>
           </div>
           ';
           }
-          // Login
-          else {
+          else {// Login
           echo '
           <input class="input-header" type="text" name="email" id="email" placeholder="Email" required>
           <input class="input-header" type="password" name="password" id="password" placeholder="Password" required>
-          <div>
-          <input style="margin-right:10px;" class="btn btn-success custom" type="submit" value="Login" formaction="login.php">
+          <div class="flex-container">
+          <div><input style="margin-right:10px;" class="btn btn-success custom" type="submit" value="Login" formaction="login.php"></div>
           </form> 
           <form method="post">
-          <input class="btn btn-danger custom" type="submit" value="Register" name="reg">
+          <div><input class="btn btn-danger custom" type="submit" value="Register" name="reg"></div>
           </form>
           </div>
           ';
